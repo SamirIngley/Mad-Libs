75cfdab39ba0adb050dd34405c754d9172c00e4b
SamirIngley <samir.ingle7@gmail.com>
2019-08-31 14:27:19 -0700

    second commit

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+
+
+
+
+print("I went to the store today to buy myself a " + noun[])
+print("I wore it to my neice's party where the " + noun[] + "balloons")
+print(" and " + adjective[] + "house were calling my name. The ")
+print(adjective[] + "show after was spectacular and full of surprises.")
+print("We spent the day " + verb[] + " cake and ")
