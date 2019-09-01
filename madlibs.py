3523fdf2970fe191b2c48a7aad53a0bfc3be7b97
SamirIngley <samir.ingle7@gmail.com>
2019-08-31 17:08:10 -0700

    third commit

--- madlibs.py
+++ madlibs.py
@@ -1,9 +1,27 @@
 
+print("Give me five single nouns: ")
+nouns0 = input()
+nouns1 = input()
+nouns2 = input()
+nouns3 = input()
+nouns4 = input()
+
+print("Gimme a plural noun: ")
+plural_nouns0 = input()
+
+print("Gimme two adjectives: ")
+adjectives0  = input()
+adjectives1 = input()
 
 
 
-print("I went to the store today to buy myself a " + noun[])
-print("I wore it to my neice's party where the " + noun[] + "balloons")
-print(" and " + adjective[] + "house were calling my name. The ")
-print(adjective[] + "show after was spectacular and full of surprises.")
-print("We spent the day " + verb[] + " cake and ")
+# def show():
+print("______________________________________________________")
+print("I went to the store today to buy myself a " + nouns0 + ".")
+print("I wore it to my neice's party where the " + nouns1 + " balloons ")
+print("and " + adjectives0 + " house were calling my name. After, the ")
+print("magic " + nouns2 + " was spectacular and full of surprises. ")
+print("We spent the day eating " + adjectives1 + " " + nouns3 + " and " + nouns4)
+print("from the pinata. We fell asleep watching " + plural_nouns0 + ".")
+
+# show()
